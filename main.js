abfc31b0c2c1bc0a41323125878f7fa8b8705742
mariana <mari.marques26@gmail.com>
2018-08-09 19:37:18 -0300

    olha ai gabriel

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-///
+// menu hamburguer mobile
+
+<script>
+    $(document).ready(function () {
+        $('ul').hover(function () {
+            $(this).find('.menu-content').slideDown();
+        }, function () {
+            $(this).find('.menu-content').slideUp();
+        });
+    });
+</script>
