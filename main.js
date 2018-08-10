0080ae485e346806e35e96d4340a5812988d10bb
mariana <mari.marques26@gmail.com>
2018-08-09 22:00:40 -0300

    espaço doido

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-// menu hamburguer mobile
+// // menu hamburguer mobile
 
 
-    $(document).ready(function () {
-        $('ul').hover(function () {
-            $(this).find('.menu-content').slideDown();
-        }, function () {
-            $(this).find('.menu-content').slideUp();
-        });
-    });
+//     $(document).ready(function () {
+//         $('ul').hover(function () {
+//             $(this).find('.menu-content').slideDown();
+//         }, function () {
+//             $(this).find('.menu-content').slideUp();
+//         });
+//     });
 
+
+//     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
