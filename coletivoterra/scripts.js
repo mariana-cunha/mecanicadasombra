19c56f5e2b7e29971db6c6e5ed34a906b37e386b
Mariana Cunha <mariana.cunha@estadao.com>
2021-02-23 15:20:21 -0300

    teste coletivo terra

---
+++ coletivoterra/scripts.js
@@ -0,0 +1,27 @@
+var swiperH = new Swiper('.swiper-container-h', {
+    spaceBetween: 0,
+    pagination: {
+    el: '.swiper-pagination-h',
+    clickable: true,
+    },
+});
+
+var swiperV = new Swiper('.swiper-container-v', {
+    direction: 'vertical',
+    spaceBetween: 0,
+    slidesPerView: 1,
+    pagination: {
+    el: '.swiper-pagination-v',
+    clickable: true,
+    },
+});
+
+var swiperV = new Swiper('.swiper-container-z', {
+    direction: 'vertical',
+    spaceBetween: 0,
+    slidesPerView: 1,
+    pagination: {
+    el: '.swiper-pagination-z',
+    clickable: true,
+    },
+});
